04236ade6c91ecfbcab8895797881782adf0fab4
jigmetdadul <jigmetdadul6@gmail.com>
2022-11-11 20:37:06 +0530

    update to LogicalExp

--- youtube/LogicalExpression1.s
+++ youtube/LogicalExpression1.s
@@ -4,5 +4,10 @@ _start:
 	MOV r1, #4
 	AND r2, r0, r1 //The logical operations takes place in binary form 001 AND 001 = 001
 	ORR r2, r0, r1 
-	EOR r2, r0, r1 //takin Exclusive OR
-	//Taking negation is not that simple 
+	EOR r2, r0, r1 //takin Exclusive OR.
+	//Taking negation is a little bit different 
+	//We use MVN(move negative)
+	//what MVN does is it moves the negation of the source to the destination
+	MOV r3, #0xFF
+	MVN r4, r3
+	
