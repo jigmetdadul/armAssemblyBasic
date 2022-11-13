147862426201e6dc1125d263c96e9f235b614606
jigmetdadul <jigmetdadul6@gmail.com>
2022-11-14 00:35:14 +0530

    just added an comment

--- youtube/addressingModes.s
+++ youtube/addressingModes.s
@@ -2,8 +2,8 @@
 _start:
        LDR r0, =list
 	   LDR r1, [r0]
-	   LDR r3, [r0, #4]!
-	   LDR r4, [r0], #4
+	   LDR r3, [r0, #4]! //post increment
+	   LDR r4, [r0], #4. //Pre increment
 	
 
 .data
